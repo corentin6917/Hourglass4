82d6c2c43fffba0c297bf34018820f1f30bcc151
Corentin Soula <corentin.soula@gmail.com>
2025-10-29 16:05:07 +0100

    Ajout du texte "Hourglass à tous!" dans la vue principale

--- Hourglass 4/ContentView.swift
+++ Hourglass 4/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hourglass!")
+            Text("Hourglass a tous!")
         }
         .padding()
     }
