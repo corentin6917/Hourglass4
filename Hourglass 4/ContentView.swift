8138d2969341aefdca03cc4b46326844c94a82f4
Corentin Soula <corentin.soula@gmail.com>
2025-10-29 15:45:37 +0100

    Initial Commit

---
+++ Hourglass 4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hourglass 4
+//
+//  Created by Corentin Soula on 29/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hourglass!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hourglass 4/Hourglass_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Hourglass_4App.swift
+//  Hourglass 4
+//
+//  Created by Corentin Soula on 29/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Hourglass_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
