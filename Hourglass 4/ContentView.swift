9c2c213b4e33c34f33483e6550f358e3d94d45d5
Corentin Soula <corentin.soula@gmail.com>
2025-10-29 16:15:04 +0100

    modif 2

--- Hourglass 4/ContentView.swift
+++ Hourglass 4/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hourglass a tous!")
+            Text("Hourglass a   ijnui tous!")
         }
         .padding()
     }
